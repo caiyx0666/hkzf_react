dd048cf23ced7444c7bb7b104ce7fc2fc8eacc8a
caiyx0666 <caiyx0666@163.com>
2020-11-25 11:45:30 +0800

    路由重定向和404页面

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { HashRouter as Router , Route} from 'react-router-dom'
+import { HashRouter as Router , Route ,Redirect ,Switch} from 'react-router-dom'
 import { Component } from 'react'
 import Home from './pages/Home'
 import CityList from './pages/CityList'
+import NotFound from './pages/NotFound'
 
 export default class App extends Component{
     render(){
       return(
           <Router>
+            <Switch>
+              <Route path="/" exact render={()=><Redirect to='/home' />} />
               <Route path="/home" component={Home} />
               <Route path="/citylist" component={CityList} />
+              <Route path="*" component={NotFound} />
+            </Switch>
           </Router>
       )
     }
