f725bda3718a85d0ab1f6282031e69392d9f4dbb
caiyx0666 <caiyx0666@163.com>
2020-11-26 19:17:28 +0800

    封装获取用户当前城市方法

---
+++ src/utils/getCityInfo.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+export default function getCityInfo() {
+    // 获取当前城市信息
+    return new Promise((resolve, reject) => {
+        const curCityStr = localStorage.getItem('cityName')
+        let curCity = null
+        if (curCityStr) {
+            curCity = JSON.parse(curCityStr)
+            resolve(curCity)
+            return
+        } else {
+            curCity = {}
+        }
+
+        try {
+            var myCity = new window.BMap.LocalCity();
+            myCity.get(async (result) => {
+                var cityName = result.name;
+                console.log(cityName);
+                const res = await axios.get(`http://localhost:8080/area/info?name=${cityName}`)
+                localStorage.setItem('cityName',JSON.stringify(res.data.body))
+                resolve(res.data.body)
+            });
+        }
+        catch(e) {
+            reject(e)
+        }
+    })
+}
