c1ee50b8a44e6083480ddeff09a7a66f336c3078
caiyx0666 <caiyx0666@163.com>
2020-11-26 23:33:54 +0800

    完成城市列表页的布局样式

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import App from './App'
 import './index.css'
 import './assets/fonts/iconfont.css'
 
+import 'react-virtualized/styles.css'; // only needs to be imported once
+
 
 ReactDom.render(<App />,document.getElementById('root'))
--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,54 +1,102 @@
 import React, { Component } from 'react'
-import { NavBar ,Icon} from 'antd-mobile';
+import { NavBar, Icon } from 'antd-mobile';
 import './index.scss'
 import axios from 'axios'
+import getCityInfo from '../../utils/getCityInfo'
+
+// 导入List组件
+import { List, AutoSizer } from 'react-virtualized';
 
 function formatCityList(list) {
     let cityList = {}
     list.forEach(city => {
-        let first = city.short.substr(0,1)
-        if(cityList[first]){
+        let first = city.short.substr(0, 1)
+        if (cityList[first]) {
             cityList[first].push(city)
-        }else{
+        } else {
             cityList[first] = [city]
         }
     })
-    
+
     const cityIndex = Object.keys(cityList).sort()
-    return{
+    return {
         cityList,
         cityIndex
     }
 }
 
+const titleHeight = 36
+const nameHeight = 50
+
 export default class CityList extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
             cityList: null,
-            cityIndex: []
+            cityIndex: [],
+            curIndex: 0
         }
     }
-    componentDidMount(){
+
+    // 渲染每一行的信息
+    rowRenderer = ({
+        key, // Unique key within array of rows
+        index, // Index of row within collection
+        isScrolling, // The List is currently being scrolled
+        isVisible, // This row is visible within the List (eg it is not an overscanned row)
+        style, // Style object to be applied to row (to position it)
+    }) => {
+        const letter = this.state.cityIndex[index]
+        const list = this.state.cityList[letter]
+        let title = ''
+        switch(letter){
+            case '#':
+                title = '当前城市'
+                break
+            case 'hot':
+                title = '热门城市'
+                break
+            default:
+                title = letter.toUpperCase()
+                break
+        }
+
+        return (
+            <div key={key} style={style} className="city">
+                <div className="title">{title}</div>
+                {list.map((item,index)=> (<div key={index} className="name">{item.label}</div>))}
+            </div>
+        );
+    }
+
+    // 获取列表每一行高度
+    getRowHeight = ({ index })=>{
+        const letter = this.state.cityIndex[index]
+        const list = this.state.cityList[letter]
+
+        return titleHeight + nameHeight * list.length
+    }
+
+    componentDidMount() {
         this.fetchCityList()
     }
 
     // 获取城市信息
-    async fetchCityList(){
+    async fetchCityList() {
         const city = await axios.get('http://localhost:8080/area/city?level=1')
-        const { cityList,cityIndex } = formatCityList(city.data.body)
+        const { cityList, cityIndex } = formatCityList(city.data.body)
 
         const hotCity = await axios.get('http://localhost:8080/area/hot')
         cityList.hot = hotCity.data.body
+        cityIndex.unshift('hot')
+        const myCityInfo = await getCityInfo()
+        cityList['#'] = [myCityInfo]
         cityIndex.unshift('#')
         this.setState({
             cityList,
             cityIndex
         })
-        console.log(cityList,cityIndex);
     }
-
-    
 
     render() {
         return (
@@ -58,6 +106,28 @@ export default class CityList extends Component {
                     icon={<Icon type="left" />}
                     onLeftClick={() => this.props.history.go(-1)}
                 >城市选择</NavBar>
+
+                <AutoSizer>
+                    {({ height,width }) => (
+                        <List
+                            width={width}
+                            height={height}
+                            rowCount={this.state.cityIndex.length}
+                            rowHeight={this.getRowHeight}
+                            rowRenderer={this.rowRenderer}
+                        />
+                    )}
+                </AutoSizer>
+
+                <ul className="city-index">
+                        {this.state.cityIndex.map((item,index) => (
+                            <li key={index} className="city-index-item">
+                                <span className={index === this.state.curIndex ? 'index-active' : ''}>
+                                    {item === 'hot' ? '热' : item.toUpperCase()}
+                                </span>
+                            </li>
+                        ))}
+                </ul>
             </div>
         )
     }
