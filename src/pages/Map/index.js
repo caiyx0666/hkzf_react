e4a883e50b8cac1b70079924b94e20fcb3207b72
caiyx0666 <caiyx0666@163.com>
2020-11-27 16:39:49 +0800

    完成Map地图的地址解析

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,18 +1,29 @@
 import { Component } from "react";
 import styles from './index.module.css'
 import NavHeader from '../../components/NavHeader'
+import getCurrentCity from '../../utils/getCityInfo'
 
 export default class Map extends Component {
-    componentDidMount(){
+    async componentDidMount() {
+        let { label } = await getCurrentCity()
         var map = new window.BMap.Map("container");
-        var point = new window.BMap.Point(116.404, 39.915); 
-        map.centerAndZoom(point, 15);
+        //创建地址解析器实例
+        var myGeo = new window.BMap.Geocoder();
+        // 将地址解析结果显示在地图上，并调整地图视野
+        myGeo.getPoint(label, function (point) {
+            if (point) {
+                map.centerAndZoom(point, 11);
+                // map.addOverlay(new window.BMapGL.Marker(point, { title: label }))
+            } else {
+                alert('您选择的地址没有解析到结果！');
+            }
+        }, label)
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className={styles.mapWrapper}>
                 <NavHeader>地图找房</NavHeader>
-                <div id="container"></div> 
+                <div id="container"></div>
             </div>
         )
     }
