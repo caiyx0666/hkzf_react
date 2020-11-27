560f2fb4b97708b80725bf242d130d85b677f164
caiyx0666 <caiyx0666@163.com>
2020-11-27 15:06:30 +0800

    对封装的NavHeader组件进行优化

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -1,16 +1,22 @@
 import { NavBar, Icon } from 'antd-mobile'
 import { withRouter } from 'react-router-dom'
+import propTypes from 'prop-types'
 import './index.scss'
 
 function NavHeader(props) {
-
+    const defaultHandle = () =>{props.history.go(-1)}
     return (
         <NavBar
             mode="light"
             icon={<Icon type="left" />}
-            onLeftClick={() => props.history.go(-1)}
+            onLeftClick={props.onLeftClick || defaultHandle}
         >{props.children}</NavBar>
     )
+}
+
+NavHeader.propTypes ={
+    children: propTypes.string.isRequired,
+    onLeftClick: propTypes.func
 }
 
 export default withRouter(NavHeader)
