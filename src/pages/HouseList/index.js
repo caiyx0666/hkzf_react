df2f97068156d8ab96e5141a30fd2ed8c296898b
caiyx0666 <caiyx0666@163.com>
2020-11-29 16:35:52 +0800

    引入Filter筛选组件模板

--- src/pages/HouseList/index.js
+++ src/pages/HouseList/index.js
@@ -1,13 +1,18 @@
 import { Component } from 'react'
 import SearchBox from '../../components/SearchBox'
 import './index.scss'
+import Filter from './components/Filter';
 export default class HouseList extends Component {
     render() {
         return (
-            <div className="houselist" >
+            <div className="houselistWrapper" >
                 <div className="header">
-                    <i className="iconfont icon-back"></i>
+                    <i className="iconfont icon-back" onClick={()=> this.props.history.go(-1)}></i>
                     <SearchBox />
+                </div>
+                <div className="houselist">
+                    {/* 筛选框 */}
+                    <Filter  />
                 </div>
             </div>
         )
